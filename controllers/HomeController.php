4dd8c63688c4656de090aa9f4b2b5b76a5e5a9c6
guillcodes <guilcodez@gmail.com>
2025-06-12 22:33:39 -0300

    feat: atualizar HomeController adicionando function home

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,21 +1,24 @@
 <?php
 
-    class HomeController{
+namespace controllers;
 
-        public static function login(){
+class HomeController {
 
-            session_start();
-
-        }
-
-        public static function logout(){
-
-            session_start();
-
-        }
-
-
+    public static function home() {
+    session_start();
+    include_once __DIR__ . '/../views/home.php';
+}
+    public static function login() {
+        session_start();
+        echo "Página de login (em construção)";
     }
 
+    public static function logout() {
+        session_start();
+        session_destroy();
+        header('Location: /PHP_A2');
+        exit;
+    }
+}
 
 ?>
