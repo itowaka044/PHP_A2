627bc2be3ae8df20692d666b282605e50dd66d6e
joao vitor wakamori <joao.waka@gmail.com>
2025-06-18 10:38:43 -0300

    ajeitando front: horarios

--- views/horarios.php
+++ views/horarios.php
@@ -1,4 +1,10 @@
+<?php
 
+include_once __DIR__ . '/includes/header.php';
+
+?>
+
+<div id="horarios">
 <h1>Horários</h1>
 
 <form action="/PHP_A2/reserva/processar-reserva" method="post">
@@ -9,18 +15,23 @@
         echo CsrfToken::hiddenHtml();
     ?>
 
-    <p>Selecione o ID do horário:</p>
+    <p>Selecione o ID do Horário:</p>
     <input type="number" name="idHorario">
 
-    <p>Selecione o ID da quadra:</p>
+    <BR>
+    <BR>
+
+    <p>Selecione o ID da Quadra:</p>
     <input type="number" name="idQuadra">
 
     <br><br>
 
-    <input type="submit">
+    <input CLASS="enviar" type="submit">
 
 </form>
+</div>
 
+<div id="mostrarHorarios">
 
 <?php
     require_once "C:\\xampp\htdocs\PHP_A2\controllers\HorarioController.php";
@@ -29,12 +40,17 @@
     $horarios = HorarioController::consultarHorarioDisp();
     
     foreach($horarios as $horario){
-        echo "idHorario: " . $horario["idHorario"] . "<BR>"
-        . "idQuadra: " . $horario["idQuadra"] . "<BR>"
-        . "dataHorario: " . $horario["dataHorario"] . "<BR>"
-        . "horaInicio: " . $horario["horaInicio"] . "<BR>"
-        . "horaFim: " . $horario["horaFim"] . "<BR>"
+        echo "<div class='horarioDiv'>"
+        . "<p class='ok'>ID do Horario: " . $horario["idHorario"] . "</p><BR>"
+        . "<p class='ok'>ID da Quadra: " . $horario["idQuadra"] . "</p><BR>"
+        . "<p>dataHorario: " . $horario["dataHorario"] . "</p><BR>"
+        . "<p>horaInicio: " . $horario["horaInicio"] . "</p><BR>"
+        . "<p>horaFim: " . $horario["horaFim"] . "</p><BR>"
+        . "</div>"
         . "<Br><br>";
+
     }
 
 ?>
+
+</div>
--- views/selecionar.php
+++ views/selecionar.php
@@ -1,28 +1,38 @@
-<form action="/PHP_A2/horario/processar-horario" method="post">
-
-    <?php
-        require_once "C:\\xampp\htdocs\PHP_A2\security\CsrfToken.php";
-        use security\CsrfToken;
-        echo CsrfToken::hiddenHtml();
-    ?>
-
-    <p>Selecione a quadra:</p>
-    <select name="id" id="">
-        <option value="1">Quadra A</option>
-        <option value="2">Quadra B</option>
-        <option value="3">Quadra C</option>
-    </select>
-    <br>
-
-    <p>Selecione o dia:</p>
-    <input type="date" name="date">
-<br><br>
-    <input type="submit">
-
-</form>
-
 <?php
 
+include_once __DIR__ . '/includes/header.php';
+
+?>
+<div id="selecionar">
+
+    <form action="/PHP_A2/horario/processar-horario" method="post">
+
+        <?php
+            require_once "C:\\xampp\htdocs\PHP_A2\security\CsrfToken.php";
+            use security\CsrfToken;
+            echo CsrfToken::hiddenHtml();
+
+            
+        ?>
+
+
+
+        <p>Selecione a quadra:</p>
+        <select name="id" id="">
+            <option value="1">Quadra A</option>
+            <option value="2">Quadra B</option>
+            <option value="3">Quadra C</option>
+        </select>
+        <br>
+
+        <p>Selecione o dia:</p>
+        <input type="date" name="date">
+        <br><br>
+        <input class="enviar" type="submit">
+
+    </form>
+</div>
+<?php
 ?>
 
 
